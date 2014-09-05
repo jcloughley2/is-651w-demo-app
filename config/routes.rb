55bf579d477f5b58373f163cb964e0f0065a4b0f
Joe Cloughley <joecloughley@gmail.com>
2014-09-04 22:36:06 -0400

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Clo2DemoApp::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
