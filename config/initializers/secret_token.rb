be3d8a2b93a570937b73011b01589fd339716c9f
Joe Cloughley <joecloughley@gmail.com>
2014-09-04 21:23:55 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clo2DemoApp::Application.config.secret_key_base = '8141cedb638d3f61474500e9d0e9425c119bd26697ce390505bcc6ddb4ec853921496a53dbb4054c78f1537759e7bc68218c5da11fc36278363112e63ab7b1f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Clo2DemoApp::Application.config.session_store :cookie_store, key: '_clo2_demo_app_session'
